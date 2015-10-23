5531ad0c9ffe8216d1c85f4bde9caae82bdde9e4
Paula Galindo <paulagalindovf@live.com>
2015-10-23 21:21:40 -0200

    Create using-expression.c

---
+++ using-expression.c
@@ -0,0 +1,29 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+
+int main()
+{ int a,media;
+a=0;
+media=0;
+while(a<100){ 
+      a++;            
+      printf("-%d",a);}
+      
+ 
+      printf("\n soma de 1-100=\n");
+      media=(101*50);
+      printf("%d\n",media);
+       
+      printf("\na divisao dessa soma por 100 = \n");
+      media= media/100;
+      printf("%d\n",media);
+      
+      
+  
+         
+    
+    
+    system("PAUSE");
+    return 0;
+}
